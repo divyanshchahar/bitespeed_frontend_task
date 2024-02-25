5049ed5b5b72894db2ce8625c4cc587bed484761
divyanshchahar <divyanshchahar@outlook.com>
2024-02-25 18:00:16 +0530

    updated styling

--- src/ui/components/CustomTextNodeComponent.jsx
+++ src/ui/components/CustomTextNodeComponent.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Handle, Position } from 'reactflow';
+import styles from './CustomTextNodeComponent.module.css';
 
 function CustomTextNodeComponent() {
 	return (
 		<>
-			<div style={{ background: 'red', padding: '5px', borderRadius: '5px' }}>
-				<p>Text Message</p>
+			<div
+				// style={{ background: 'red', padding: '5px', borderRadius: '5px' }}
+				className={styles.container}
+			>
+				<p className={styles.paragraph}>Text Message</p>
 				<input
 					type="text"
 					id="text"
 					placeholder="input text mesage"
-					className="nodrag"
-					style={{ padding: '5px' }}
+					// className="nodrag"
+					className={styles.input}
+					// style={{ padding: '5px' }}
 				/>
 			</div>
 			<Handle type="target" position={Position.Left} />
