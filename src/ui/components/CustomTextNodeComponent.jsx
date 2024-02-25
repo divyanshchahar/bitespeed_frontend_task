584d32efd74fb2e08fd27eb4fcbb467e227d17b5
divyanshchahar <divyanshchahar@outlook.com>
2024-02-25 18:07:49 +0530

    cleaned up code - removed unused code - updated styling - removed unused classes

--- src/ui/components/CustomTextNodeComponent.jsx
+++ src/ui/components/CustomTextNodeComponent.jsx
@@ -5,18 +5,13 @@ import styles from './CustomTextNodeComponent.module.css';
 function CustomTextNodeComponent() {
 	return (
 		<>
-			<div
-				// style={{ background: 'red', padding: '5px', borderRadius: '5px' }}
-				className={styles.container}
-			>
+			<div className={styles.container}>
 				<p className={styles.paragraph}>Text Message</p>
 				<input
 					type="text"
 					id="text"
 					placeholder="input text mesage"
-					// className="nodrag"
-					className={styles.input}
-					// style={{ padding: '5px' }}
+					className={`nodrag ${styles.input}`}
 				/>
 			</div>
 			<Handle type="target" position={Position.Left} />
