4255d4a9933b69f1a3bdcbd678cc0283fd54e41a
divyanshchahar <divyanshchahar@outlook.com>
2024-02-27 16:05:11 +0530

    added new list

---
+++ src/react-flow/nodes.js
@@ -0,0 +1,10 @@
+import CustomTextNodeLayout from '../ui/layouts/CustomTextNodeLayout';
+
+export const nodeTypes = { customTextNode: CustomTextNodeLayout };
+
+export const nodesList = [
+	{
+		name: 'customTextNode',
+		displayText: 'Add custom naode type',
+	},
+];
