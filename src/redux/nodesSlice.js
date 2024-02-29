e5f82a67ef754eace6a6a97fe7e97a20944de23d
divyanshchahar <divyanshchahar@outlook.com>
2024-02-29 13:16:19 +0530

    added new reducer

--- src/redux/nodesSlice.js
+++ src/redux/nodesSlice.js
@@ -11,9 +11,18 @@ const nodesSlice = createSlice({
 		nodeUpdated(state, action) {
 			return applyNodeChanges(action.payload, state);
 		},
+		textUpdated(state, action) {
+			const { id, data } = action.payload;
+			return state.map((node) => {
+				if (node.id === id) {
+					return { ...node, data: data };
+				}
+				return node;
+			});
+		},
 	},
 });
 
-export const { nodeAdded, nodeUpdated } = nodesSlice.actions;
+export const { nodeAdded, nodeUpdated, textUpdated } = nodesSlice.actions;
 
 export default nodesSlice.reducer;
