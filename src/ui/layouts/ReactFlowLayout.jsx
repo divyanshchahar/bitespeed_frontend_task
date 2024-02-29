3886283b559fbf5605144437d87cc884c2646269
divyanshchahar <divyanshchahar@outlook.com>
2024-02-29 12:31:05 +0530

    removed commented code

--- src/ui/layouts/ReactFlowLayout.jsx
+++ src/ui/layouts/ReactFlowLayout.jsx
@@ -5,44 +5,20 @@ import 'reactflow/dist/style.css';
 import { v4 as uuidv4 } from 'uuid';
 import { nodeTypes } from '../../react-flow/nodes';
 import { edgesAdded, edgesDeleted } from '../../redux/edgesSlice';
+import { nodeUpdated } from '../../redux/nodesSlice';
 import CustomEdgeComponenet from '../components/CustomEdgeComponenet';
 import styles from './ReactFlowLayout.module.css';
 import SidePanel from './SidePanel';
-import { nodeUpdated } from '../../redux/nodesSlice';
-
-// // mock list of nodes
-// const initialNodes = [
-// 	{
-// 		id: 'node-1',
-// 		type: 'customTextNode',
-// 		position: { x: 0, y: 0 },
-// 	},
-// 	{
-// 		id: 'node-2',
-// 		type: 'customTextNode',
-// 		position: { x: 100, y: 100 },
-// 	},
-// 	{
-// 		id: 'node-3',
-// 		type: 'customTextNode',
-// 		position: { x: 200, y: 200 },
-// 	},
-// ];
 
 const edgeTypes = { customEdgeComponent: CustomEdgeComponenet };
 
 function ReactFlowLayout() {
-	// const [nodes, setNodes] = useState(initialNodes);
-
 	const dispatch = useDispatch();
 	const edges = useSelector((state) => state.edges);
 	const nodes = useSelector((state) => state.nodes);
 
 	// function to handle node changes like dragging ang deletion
 	const onNodesChange = useCallback(
-		// (changes) => {
-		// 	console.log(changes);
-		// },
 		(changes) => {
 			dispatch(nodeUpdated(changes));
 		},
