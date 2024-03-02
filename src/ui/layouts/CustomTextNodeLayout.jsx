b88f95d82ed56cf2aadcee013582ea880182c4b9
divyanshchahar <divyanshchahar@outlook.com>
2024-03-02 12:43:59 +0530

    prettified code - rearranged imports - added comments for clarity

--- src/ui/layouts/CustomTextNodeLayout.jsx
+++ src/ui/layouts/CustomTextNodeLayout.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import styles from './CustomTextNodeLayout.module.css';
-
 import CustomSourceHandleComponent from '../components/CustomSourceHandleComponent';
 import CustomTargetHandleComponent from '../components/CustomTargetHandleComponent';
 
+// React componenet to render Text Node (Message Node)
 function CustomTextNodeComponent({ data }) {
 	return (
 		<>
